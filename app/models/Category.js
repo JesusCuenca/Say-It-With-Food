0f74f113910e77c862ae18f43014a7dbb4243ab4
Jesús Cuenca <jesus.cuenca.lo@gmail.com>
2015-05-29 19:52:29 +0200

    Added models.

---
+++ app/models/Category.js
@@ -0,0 +1,38 @@
+exports.definition = {
+	config: {
+		columns: {
+		    "id": "INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT",
+		    "name": "TEXT",
+		    "slug": "TEXT"
+		},
+		adapter: {
+			type: "sql",
+			collection_name: "Category",
+			idAttribute: "id"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+			// extended functions and properties go here
+
+			// For Backbone v1.1.2, uncomment the following to override the
+			// fetch method to account for a breaking change in Backbone.
+			/*
+			fetch: function(options) {
+				options = options ? _.clone(options) : {};
+				options.reset = true;
+				return Backbone.Collection.prototype.fetch.call(this, options);
+			}
+			*/
+		});
+
+		return Collection;
+	}
+};
---
+++ app/models/Recipe.js
@@ -0,0 +1,43 @@
+exports.definition = {
+	config: {
+		columns: {
+		    "id": "INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT",
+		    "title": "TEXT",
+		    "desc": "TEXT",
+		    "category": "INTEGER",
+		    "ingredients": "TEXT",
+		    "time": "TEXT",
+		    "difficulty": "INTEGER",
+		    "likes": "INTEGER"
+		},
+		adapter: {
+			type: "sql",
+			collection_name: "Recipe",
+			idAttribute: "id"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			// extended functions and properties go here
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+			// extended functions and properties go here
+
+			// For Backbone v1.1.2, uncomment the following to override the
+			// fetch method to account for a breaking change in Backbone.
+			/*
+			fetch: function(options) {
+				options = options ? _.clone(options) : {};
+				options.reset = true;
+				return Backbone.Collection.prototype.fetch.call(this, options);
+			}
+			*/
+		});
+
+		return Collection;
+	}
+};
