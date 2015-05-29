6e31a58dbe9a402aff9b1503944903b074a0c860
Jesús Cuenca <jesus.cuenca.lo@gmail.com>
2015-05-30 00:00:35 +0200

    Styling category view. Edit index.js so category view opens when a category is clicked.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,10 @@
 var abx = require ('com.alcoapps.actionbarextras');
 var colors = Alloy.Globals.colors;
 
+/**
+ * Configure action bar styles with the Action Bar
+ * Extras Plugin.
+ */
 function configureABX (_event) {
 	
 	abx.title = {
@@ -14,11 +18,12 @@ function configureABX (_event) {
 $.index.addListener('open', configureABX);
 
 
-
-
-
 function openCategory(_event) {
-    alert(_event.source.model);
+	
+	var catId = _event.source.model;
+    
+    var categoryView = Alloy.createController('category', catId).getView();
+    categoryView.open();    
 }
 
 $.index.open();
